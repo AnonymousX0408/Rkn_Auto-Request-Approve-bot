4264c23f25e45a7f571b5d8b5e5b6c1585790d36
Hi <142774957+AnonymousX0408@users.noreply.github.com>
2024-03-13 12:31:44 +0530

    Update add.py

--- RknDeveloper/untils/add.py
+++ RknDeveloper/untils/add.py
@@ -7,7 +7,7 @@ from pyrogram.types import BotCommand
 async def set_commands(app):
     RKN = [
         BotCommand("start", "Check Bot Alive."),
-        BotCommand("rknusers", "Total Users Check.(Aᴅᴍɪɴ Oɴʟʏ)"),
+        BotCommand("users", "Total Users Check.(Aᴅᴍɪɴ Oɴʟʏ)"),
         BotCommand("broadcast", "Broadcast Massage Send All Users In Bot (Aᴅᴍɪɴ Oɴʟʏ)."),
         BotCommand("fd_broadcast", "Broadcast massage Forward Not Remove (Aᴅᴍɪɴ Oɴʟʏ)."),
         BotCommand("restart", "Restarts or re-deploys the server (Aᴅᴍɪɴ Oɴʟʏ).")
