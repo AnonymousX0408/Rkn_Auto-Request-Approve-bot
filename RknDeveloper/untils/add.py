3521a36a0792847ff9522ad16418aa03ccd3442d
Hi <142774957+AnonymousX0408@users.noreply.github.com>
2024-03-13 12:32:25 +0530

    Update add.py

--- RknDeveloper/untils/add.py
+++ RknDeveloper/untils/add.py
@@ -10,7 +10,7 @@ async def set_commands(app):
         BotCommand("users", "Total Users Check.(Aᴅᴍɪɴ Oɴʟʏ)"),
         BotCommand("broadcast", "Broadcast Massage Send All Users In Bot (Aᴅᴍɪɴ Oɴʟʏ)."),
         BotCommand("fd_broadcast", "Broadcast massage Forward Not Remove (Aᴅᴍɪɴ Oɴʟʏ)."),
-        BotCommand("restart", "Restarts or re-deploys the server (Aᴅᴍɪɴ Oɴʟʏ).")
+        BotCommand("restart", "To Restart The Bot (Aᴅᴍɪɴ Oɴʟʏ).")
  
     ]
     await app.set_bot_commands(RKN)
